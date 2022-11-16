841abfbc3c31b7435d15a364dd4989f5d388abd0
Hasib Ullah <hasibullah04@gmail.com>
2022-11-16 13:13:00 +0600

    assets updated. working on game analytics component

---
+++ src/app/constants/analytics.ts
@@ -0,0 +1,19 @@
+import { GameAnalytics } from '../interfaces/game-analytics.interface';
+
+export const gameAnalytics: GameAnalytics[] = [
+  {
+    logoPath: '../../assets/images/player-base-logo.svg',
+    analyticsTitle: 'Player Base',
+    backgroundColor: '#FFECEC',
+  },
+  {
+    logoPath: '../../assets/images/winners-logo.svg',
+    analyticsTitle: 'Winners',
+    backgroundColor: '#EBF4FF',
+  },
+  {
+    logoPath: '../../assets/images/high-scorers-logo.svg',
+    analyticsTitle: 'High Scorers',
+    backgroundColor: '#FFF8E4',
+  },
+];
---
+++ src/app/interfaces/game-analytics.interface.ts
@@ -0,0 +1,5 @@
+export interface GameAnalytics {
+  logoPath: string;
+  analyticsTitle: string;
+  backgroundColor: string;
+}
