18f15abb0b46d464dc291566c0623e67a569ac7c
Hasib Ullah <hasibullah04@gmail.com>
2022-11-21 23:38:44 +0600

    generate winners modal config done

--- src/app/game-analytics/winners/modals/generate-winners-modal/generate-winners-modal.component.ts
+++ src/app/game-analytics/winners/modals/generate-winners-modal/generate-winners-modal.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-generate-winners-modal',
   templateUrl: './generate-winners-modal.component.html',
   styleUrls: ['./generate-winners-modal.component.scss'],
 })
-export class GenerateWinnersModalComponent implements OnInit {
+export class GenerateWinnersModalComponent implements OnInit, OnDestroy {
   form: FormGroup;
   maxDate = new Date(new Date().setFullYear(new Date().getFullYear()));
   timeCriteria: string = 'alltime';
+  timeSpanSubscription: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<GenerateWinnersModalComponent>,
@@ -25,7 +27,9 @@ export class GenerateWinnersModalComponent implements OnInit {
       limit: [10],
     });
 
-    this.form.controls['timeSpan'].valueChanges.subscribe((val) => {
+    this.timeSpanSubscription = this.form.controls[
+      'timeSpan'
+    ].valueChanges.subscribe((val) => {
       switch (val) {
         case 'ALLTIME':
           this.timeCriteria = 'alltime';
@@ -40,7 +44,15 @@ export class GenerateWinnersModalComponent implements OnInit {
     });
   }
 
+  sendGenerateWinnersFilter() {
+    console.log(this.form.getRawValue());
+  }
+
   closeModal() {
     this.dialogRef.close();
+  }
+
+  ngOnDestroy(): void {
+    this.timeSpanSubscription.unsubscribe();
   }
 }
